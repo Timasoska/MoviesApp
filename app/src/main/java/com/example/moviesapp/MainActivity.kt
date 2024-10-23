b926a1abf28a0d7545199bf7eb6a5aa5da802cd3
Timofey <Tim005rud@gmail.com>
2024-10-23 12:03:20 +0300

    Добавил обучалку

--- app/src/main/java/com/example/moviesapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.moviesapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,22 +16,41 @@ import com.example.moviesapp.presentation.MovieDetail.MovieDetailViewModel
 import com.example.moviesapp.presentation.ui.MainPage
 import com.example.moviesapp.presentation.ui.theme.MoviesAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import android.content.Context
+import android.widget.Toast
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val viewModel: MovieViewModel by viewModels()
     private val detailViewModel: MovieDetailViewModel by viewModels()
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MoviesAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) {
                     val navController = rememberNavController()
                     CustomNavHost(navController = navController, movieViewModel = viewModel, movieDetailViewModel = detailViewModel)
-                    //MainPage(movieViewModel = viewModel, movieDetailViewModel = detailViewModel)
+
+                    val sharedPreferences = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+                    val isFirstLaunch = sharedPreferences.getBoolean("isFirstLaunch", true)
+
+                    if (isFirstLaunch) {
+                        // Показываем Toast с инструкцией
+                        Toast.makeText(this, "Swipe left or right to navigate between pages!", Toast.LENGTH_LONG).show()
+
+                        // После показа обновляем значение, чтобы больше не показывалось
+                        sharedPreferences.edit().putBoolean("isFirstLaunch", false).apply()
+                    }
                 }
             }
         }
     }
 }
+
+
+
+
+
+
